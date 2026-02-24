f417d356a0ae2d352f7cefb7cf1b4a06a71b4a56
Venkat-1819 <venkatkiran1819@gmail.com>
2026-02-24 11:22:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import random
-import os
 
 app = Flask(__name__)
 
@@ -9,7 +8,3 @@ def home():
     temperature = random.randint(20, 35)
     humidity = random.randint(30, 70)
     return render_template("index.html", temp=temperature, hum=humidity)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
